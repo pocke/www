cf7fa34284f3daa05471af6092592e17283a16ac
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-14 11:56:39 +0900

    Initialize commit.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/skratchdot/open-golang/open"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr())
+	open.Run(url)
+	fmt.Println(url)
+
+	http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "."+r.URL.Path)
+	}))
+}
